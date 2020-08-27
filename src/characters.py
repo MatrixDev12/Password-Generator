f820623533dd9688be37a33c3112f25a99fe40fb
MatrixDev12 <MatrixDev@tutanota.com>
2020-08-27 11:38:41 +0100

    initial commit

---
+++ src/characters.py
@@ -0,0 +1,2 @@
+character = "djksaldj@#$ldsjaklfdlsfjsdfdjskladjasdj$#@$#@$2@!#FDSWFDSFFDSFQWERTYUIOPLKJHGFDSAZXCVBNM!@#$%^&*()_+;[]sd432432442425435435654657868455432432|\/"
+
---
+++ src/generator.py
@@ -0,0 +1,33 @@
+import random
+import time
+from characters import character
+
+characters = character
+
+number = input('Number of passwords? - ')                
+number = int(number)
+
+time.sleep(1)
+
+length = input('How many characters? - ')
+length = int(length)
+
+time.sleep(0.5)
+
+print("Generating passwords...")
+
+time.sleep(1)
+
+for p in range(number):
+  password = ''
+  for c in range(length):
+    password += random.choice(characters)
+  print("password :- " + password)
+
+time.sleep(1)
+
+print("Passwords have been generated!")
+
+time.sleep(1)
+
+print("Thank you for using the MatrixDev password generator.")
